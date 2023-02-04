23566525f488d4019b73cd9e0e670a01b0b47d11
daraivanova <kudiyarova.1975@gmail.com>
2023-02-04 20:11:21 +0300

    A mock geo was created

---
+++ src/app/core/mock-geo.ts
@@ -0,0 +1,27 @@
+import { Geo } from './geo';
+
+export const Geos: Geo[] = [
+  {
+    en: 'Moscow',
+    ru: 'Москва'
+  },
+  {
+    en: 'Saint petersburg',
+    ru: 'Санкт-Петербург'
+  },
+  {
+    en: 'Ekaterinburg',
+    ru: 'Екатеринбург'
+  },
+  {
+    en: 'Vladivostok',
+    ru: 'Владивосток'
+  }
+];
+
+export const Cities: Record<string, string> = {
+  'Moscow': 'Москва',
+  'Saint Petersburg': 'Санкт-Петербург',
+  'Ekaterinburg': 'Екатеринбург',
+  'Vladivostok': 'Владивосток'
+}
